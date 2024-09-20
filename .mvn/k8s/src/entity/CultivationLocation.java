577f7da78aa83556e86f0ff8c5dac04a206fefa6
PavlosGeorgakis <it22123@hua.gr>
2024-09-20 23:33:59 +0300

    Update CultivationLocation.java

--- .mvn/k8s/src/entity/CultivationLocation.java
+++ .mvn/k8s/src/entity/CultivationLocation.java
@@ -1,1 +1,50 @@
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Entity
+@Table(name = "cultivation_locations")
+@Getter
+@Setter
+@ToString
+public class CultivationLocation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer id;
+
+    @Column
+    private String address;
+
+    @Column
+    private String area;
+
+    @Column
+    private String zipCode;
+
+    @JsonIgnore
+    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH}, mappedBy = "cultivationLocations")
+    private List<Cooperative> cooperatives;
+
+    public CultivationLocation() {
+    }
+
+    public CultivationLocation(String address, String area, String zipCode) {
+        this.address = address;
+        this.area = area;
+        this.zipCode = zipCode;
+    }
+
+    @PreRemove
+    private void preRemove() {
+        for (Cooperative cooperative : cooperatives) {
+            cooperative.removeCultivationLocation(this);
+        }
+    }
+}
 
