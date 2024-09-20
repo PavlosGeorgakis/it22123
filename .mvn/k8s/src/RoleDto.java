46ad9f3142c7a12c9d5934a049b14b5e88a78602
PavlosGeorgakis <it22123@hua.gr>
2024-09-20 23:28:23 +0300

    Update RoleDto.java

--- .mvn/k8s/src/RoleDto.java
+++ .mvn/k8s/src/RoleDto.java
@@ -1,1 +1,25 @@
+public class RoleDto {
+    private Integer id;
+
+    private RoleDto(Builder builder) {
+        this.id = builder.id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public static class Builder {
+        private Integer id;
+
+        public Builder setId(Integer id) {
+            this.id = id;
+            return this;
+        }
+
+        public RoleDto build() {
+            return new RoleDto(this);
+        }
+    }
+}
 
