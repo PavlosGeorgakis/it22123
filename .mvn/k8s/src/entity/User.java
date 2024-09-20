1e948444511cd0363e99391bebe869dcec100c06
PavlosGeorgakis <it22123@hua.gr>
2024-09-20 23:38:16 +0300

    Update User.java

--- .mvn/k8s/src/entity/User.java
+++ .mvn/k8s/src/entity/User.java
@@ -1,1 +1,76 @@
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "username"),
+                @UniqueConstraint(columnNames = "email")
+        })
+@Getter
+@Setter
+@ToString
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String vat;
+
+    @NotBlank
+    @Size(max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
+    @JsonProperty(access = Access.WRITE_ONLY)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    @JsonIgnore
+    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH}, mappedBy = "farmers")
+    private List<Cooperative> cooperatives;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL)
+    private List<Cooperative> applications;
+
+    @PreRemove
+    private void preRemove() {
+        for (Cooperative application : applications) {
+            application.setEmployee(null);
+        }
+        for (Cooperative cooperative : cooperatives) {
+            cooperative.removeFarmer(this);
+        }
+    }
+}
 
