77e7a7c3e6f3c0bb0fdce4fd6b02dafb652457ef
PavlosGeorgakis <it22123@hua.gr>
2024-09-20 23:36:44 +0300

    Update Role.java

--- .mvn/k8s/src/entity/Role.java
+++ .mvn/k8s/src/entity/Role.java
@@ -1,1 +1,27 @@
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@ToString
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 20)
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+}
 
