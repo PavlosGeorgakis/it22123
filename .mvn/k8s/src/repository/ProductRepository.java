b618489b70b64e03be95a346efd80fac851f8ccb
PavlosGeorgakis <it22123@hua.gr>
2024-09-20 23:44:49 +0300

    Update ProductRepository.java

--- .mvn/k8s/src/repository/ProductRepository.java
+++ .mvn/k8s/src/repository/ProductRepository.java
@@ -1,1 +1,15 @@
+import gr.hua.agricoop.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    // Παράδειγμα custom query για εύρεση προϊόντων με βάση το όνομα
+    @Query("SELECT p FROM Product p WHERE p.name LIKE %:name%")
+    List<Product> findByNameContaining(String name);
+}
 
