087b03819ebf2d0949b95ba43fe267cf2df3e62b
PavlosGeorgakis <it22123@hua.gr>
2024-09-20 23:35:46 +0300

    Update Product.java

--- .mvn/k8s/src/entity/Product.java
+++ .mvn/k8s/src/entity/Product.java
@@ -1,1 +1,48 @@
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import java.util.List;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@ToString
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String category;
+
+    @Column
+    private Double price;
+
+    @JsonIgnore
+    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH}, mappedBy = "products")
+    private List<Cooperative> cooperatives;
+
+    public Product() {}
+
+    public Product(String name, String category, Double price) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    @PreRemove
+    private void preRemove() {
+        for (Cooperative cooperative : cooperatives) {
+            cooperative.removeProduct(this);
+        }
+    }
+}
 
