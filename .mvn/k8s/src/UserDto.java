e50a30c275875c85088908ac0cddfda5477100d4
PavlosGeorgakis <it22123@hua.gr>
2024-09-20 23:29:33 +0300

    Update UserDto.java

--- .mvn/k8s/src/UserDto.java
+++ .mvn/k8s/src/UserDto.java
@@ -1,1 +1,25 @@
+public class UserDto {
+    private Long id;
+
+    private UserDto(Builder builder) {
+        this.id = builder.id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public static class Builder {
+        private Long id;
+
+        public Builder setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public UserDto build() {
+            return new UserDto(this);
+        }
+    }
+}
 
