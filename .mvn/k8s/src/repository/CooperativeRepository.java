610f2f73a199cf33bd6b1b7f16995f7ca532e4b4
PavlosGeorgakis <it22123@hua.gr>
2024-09-20 23:42:05 +0300

    Update CooperativeRepository.java

--- .mvn/k8s/src/repository/CooperativeRepository.java
+++ .mvn/k8s/src/repository/CooperativeRepository.java
@@ -1,1 +1,37 @@
+import gr.hua.agricoop.dto.CooperativeDto;
+import gr.hua.agricoop.entity.Cooperative;
+import gr.hua.agricoop.entity.CultivationLocation;
+import gr.hua.agricoop.entity.Product;
+import gr.hua.agricoop.entity.User;
+import gr.hua.agricoop.repository.CooperativeRepository;
+import gr.hua.agricoop.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+public class CooperativeService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private CooperativeRepository cooperativeRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private CultivationLocationService cultivationLocationService;
+
+    @Transactional
+    public List<Cooperative> get
 
